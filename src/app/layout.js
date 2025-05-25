4e403d74cb14f7e2600e00548b8817b631db0d3a
fahry firdaus <ffahry169@gmail.com>
2025-05-25 15:50:32 +0700

    config: setup font outfit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -20,9 +20,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="font-outfit">
         {children}
+        </div>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["var(--font-outfit)", "sans-serif"],
+        geistMono: ["var(--font-geist-mono)", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
